aec46df52d5b8abf05decd95c688452a6eadf6d1
Rintaro Sekino <rintaro.sekino@ntti3.com>
2018-07-24 16:32:25 -0700

    Deploy test

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello World! Shohei won!!")
+	io.WriteString(w, "Hello World! Pipeline is updated.")
 }
 
 func main() {
