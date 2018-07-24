f8a464d98f1a7ac5e2b7f7893b16a433dcb24200
Rintaro Sekino <rintaro.sekino@ntti3.com>
2018-07-23 17:01:22 -0700

    Pipeline test with manual judgement

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello World! Spinnaker!!")
+	io.WriteString(w, "Hello World! Spinnaker!! Prod Pipeline Test!!")
 }
 
 func main() {
